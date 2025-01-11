3e5668d7cd0f8ab9c2d27b4591583e3131185728
Stefan Koster <github.zp7kf@sr.dekosters.nl>
2025-01-11 16:31:50 +0100

    Removed sound_options (no longer used)

--- custom_components/homewizard_vacuum/switch.py
+++ custom_components/homewizard_vacuum/switch.py
@@ -12,8 +12,6 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.core import HomeAssistant
 from homeassistant.components.switch import SwitchEntity
 
-SOUND_OPTIONS = ["Beeps", "English", "No sound"]
-
 _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(
